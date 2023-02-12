7b2ed3beeb74801a13608939ebb6e1c35f653686
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2023-02-12 17:19:11 +0300

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,93 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
+import fsPromises from 'fs/promises';
+import path from 'path'
+import { useEffect, useState } from 'react';
 
-export default function Home() {
+export default function Home(props) {
+  const { data } = props;
+  const [tableData, setTableData] = useState(data);
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    const result = data.filter(item => item.url.includes(value));
+    setTableData(result);
+  };
+
+  const debounce = (func, delay) => {
+    let inDebounce;
+    return function () {
+      const context = this;
+      const args = arguments;
+      clearTimeout(inDebounce);
+      inDebounce = setTimeout(() => func.apply(context, args), delay);
+    }
+  };
+
+  const handleSearchDebounce = debounce(handleSearch, 500);
+
+  useEffect(() => {
+    const searchInput = document.getElementById('#search');
+    searchInput.addEventListener('input', handleSearchDebounce);
+    return () => {
+      searchInput.removeEventListener('input', handleSearchDebounce);
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Yardım Teyit</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles['search-container']}>
+          <input type='text' id='#search' placeholder='Search...' />
+          <button>Search</button>
+        </div>
+        <div className={styles['table-container']}>
+          <table>
+            <thead>
+              <tr>
+                <th>URL</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((item, index) => (
+                <tr key={index} className={item.status === 'FRAUD' ? 'fraud' : ''}>
+                  <td><a href={item.status === "FRAUD" ? "#" : item.url} target='_blank'>{item.url}</a></td>
+                  <td><span className={item.status === 'FRAUD' ? styles.status + ' fraud' : styles.status + ' secure' }>{item.status}</span></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </main>
-
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
       </footer>
-
       <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .fraud {
+          color: red;
         }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
+        .secure {
+          color: green;
         }
       `}</style>
     </div>
   )
+}
+
+// inline styles
+
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'json/data.json');
+  const jsonData = await fsPromises.readFile(filePath);
+  const objectData = JSON.parse(jsonData);
+
+  return {
+    props: objectData
+  }
 }
